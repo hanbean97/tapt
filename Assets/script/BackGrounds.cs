62bd2b77955f91a9c32fef3c6f2aa7a233155349
hanbean97 <coldbean97@gmail.com>
2024-10-17 08:38:32 +0900

    123

--- Assets/script/BackGrounds.cs
+++ Assets/script/BackGrounds.cs
@@ -6,7 +6,6 @@ public class BackGrounds : MonoBehaviour
 {
     [SerializeField] Sprite[] SpaceWorld;
     SpriteRenderer sprR;
-    Vector2 offsets;
     [SerializeField] float speed =1;
     int nowBackstage=0;
     int nowLevel =0;
@@ -16,17 +15,6 @@ public class BackGrounds : MonoBehaviour
     }
     void Update()
     {
-       if(GameManager.Instance.gamestart == true &&nowLevel != EnemysManager.Instance.level)
-        {
-            nowBackstage++;
-            if(nowBackstage >= SpaceWorld.Length)
-            {
-                nowBackstage = 0;
-            }
-            sprR.sprite = SpaceWorld[nowBackstage];
-            nowLevel = EnemysManager.Instance.level;
-        }
-       offsets.y += speed*Time.deltaTime;
-        sprR.material.SetTextureOffset("_MainTex",offsets);
+
     }
 }
