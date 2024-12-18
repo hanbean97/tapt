61b2e74ce12e637b7eea6c992a8d1490fdd2de4d
hanbean97 <coldbean97@gmail.com>
2024-12-18 19:45:33 +0900

    fdh

--- Assets/script/SoundManager.cs
+++ Assets/script/SoundManager.cs
@@ -9,15 +9,18 @@ public class SoundManager : PersistentSingleton<SoundManager>
     [SerializeField] AudioClip bgmcilp;
     AudioSource bgmPlayer;
     [Header("SFX")]
-    [SerializeField] AudioClip sfxcilp;
+    [SerializeField] AudioClip[] sfxcilp;
     public int channels;//동시에 소리가 나올수있는 최대수
     int channelIndex;
     AudioSource[] sfxPlayer;
+
+    public enum Sfx { }
 
     private void Start()
     {
         Init();
     }
+
     void Init()
     {
         // 배경음 플레이어 초기화
@@ -27,6 +30,7 @@ public class SoundManager : PersistentSingleton<SoundManager>
         bgmPlayer.playOnAwake = false;
         bgmPlayer.loop = true;
         bgmPlayer.volume = Volume;
+        bgmPlayer .clip = bgmcilp;
         // 효과음 플레이어 초기화
         GameObject sfxObj = new GameObject("sfxPlayer");
         sfxObj.transform.parent = transform;
@@ -36,6 +40,20 @@ public class SoundManager : PersistentSingleton<SoundManager>
             sfxPlayer[i] = sfxObj.AddComponent<AudioSource>();
             sfxPlayer[i].playOnAwake=false;
             sfxPlayer[i].volume = Volume;
+        }
+    }
+    public void PlaySfx(Sfx sfx)
+    {
+        for(int index =0; index < sfxPlayer.Length; index++)
+        {
+            int LoopIndex = (index + channelIndex) % sfxPlayer.Length;
+            if (sfxPlayer[LoopIndex].isPlaying)
+                continue;
+
+            channelIndex = LoopIndex;
+            sfxPlayer[LoopIndex].clip = sfxcilp[(int)sfx];
+            sfxPlayer[LoopIndex].Play();
+            break;
         }
     }
 }
