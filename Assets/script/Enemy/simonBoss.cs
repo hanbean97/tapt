ef3f4c01778ab19612ed8f111e7405f80b6efa95
hanbean97 <coldbean97@gmail.com>
2025-01-01 20:48:07 +0900

    fasdfsaf

--- Assets/script/Enemy/simonBoss.cs
+++ Assets/script/Enemy/simonBoss.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class simonBoss : BossEnmey
 {
-    [SerializeField] GameObject[] ball;
+    [SerializeField] Transform[] ball;
+    [SerializeField] float balldistance;
+    [SerializeField] float ballspeed;
     private void Start()
     {
         base.bossType = BossType.sinmon;
@@ -12,6 +14,25 @@ public class simonBoss : BossEnmey
     public override void Update()
     {
         base.Update();
-        
+        for (int i = 0; i < ball.Length; i++)
+        {
+            switch(i%2)
+            {
+                case 0:
+                  //n-dis
+                    break;
+                case 1:
+                    break; 
+
+            }
+
+           // ball[i].transform 
+        }
+       
+    }
+
+    float DisCalcul()
+    {
+        return 0;
     }
 }
